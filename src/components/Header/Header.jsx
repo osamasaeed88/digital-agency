21ca226195021c05e9002ef9d5ead37ebbd45d60
osamasaeed <osamasaeed444@hotmail.com>
2022-10-30 22:12:28 +0500

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+import "./header.css";
+
+const nav_links =[
+    {
+        path:'#home',
+        display:'Home'
+    },
+    {
+        path:'#about',
+        display:'About'
+    },
+    {
+        path:'#services',
+        display:'Services'
+    },
+    {
+        path:'#projects',
+        display:'Projects'
+    },
+    {
+        path:'#blog',
+        display:'Blog'
+    },
+]
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="container">
+                <div className="nav__wrapper">
+                    <div className="logo">
+                        <h2>Digi-World</h2>
+                        <p>Lets Grow Furthur Tohether!</p>
+                    </div>
+                    {/* ===== navigation ===== */}
+                    <div className="navigation">
+                        <ul className="menu">
+                        {
+                            nav_links.map((item,index) => (
+                                <li className="menu__item"><a href={item.path} 
+                                className="menu__link">{item.display}</a></li>
+                            ))
+                        }
+                        </ul>
+                    </div>
+
+                    {/* ===== light mode ===== */}
+                    <div className="light__mode">
+                        <span><i class="ri-sun-line"></i>Light Mode</span>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
+
