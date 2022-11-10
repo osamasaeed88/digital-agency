1c67ab1ec0bb2c40365a90a25b74123ba88c177d
osamasaeed88 <osamasaeed444@hotmail.com>
2022-11-11 02:46:56 +0500

    latest

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -31,7 +31,7 @@ const Header = () => {
             <div className="container">
                 <div className="nav__wrapper">
                     <div className="logo">
-                        <h2>SDP-Tech</h2>
+                        <h2>SDP-hub</h2>
                         <p>Lets Grow Furthur Together Always!</p>
                     </div>
                     {/* ===== navigation ===== */}
