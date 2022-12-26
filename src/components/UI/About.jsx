6facec46ff07437abc83fab7d0a411ad10a2a05f
Osama Saeed <osamasaeed444@hotmail.com>
2022-12-26 23:13:40 +0500

    Add Some responsiveness to different fields

--- src/components/UI/About.jsx
+++ src/components/UI/About.jsx
@@ -17,40 +17,39 @@ const chooseData = [
         icon: 'ri-customer-service-2-line',
         title: '24/7 Support',
         description: 'Lorem ipsum. Neque earum quod aspernatur exercitationem illum incidunt harum consectetur.',
- }
+    }
 ]
 
 const About = () => {
-  return (
-    <section id="about">
-        <div className="container">
-            <div className="about_wrapper">
-                <div className="about_content">
-                   <h6 className="subtitle">Why Choosing Us</h6>
-                   <h2>We are Team Of Professionals</h2>
-                   <h2 className="highlight">Lets Grow Further Towards Future</h2>
-                   <p className="description about_content-desc">Lorem ipsum. Neque earum quod aspernatur exercitationem illum incidunt harum consectetur.</p>
-                
-                <div className="choose_item-wrapper">
-                    {
-                        chooseData.map((item,index)=>(
-                            <div className="choose_us-item" key={index}>
-                    <span className="choose_us-icon"><i class={item.icon}></i></span>
-                <div>
-                    <h4 className="choose_us-title">{item.title}</h4>
-                    <p className="description">{item.description}</p>
-                </div>
-                </div>
-                        ))
-                    }
-                </div>
-                </div>
+    return (
+        <section id="about">
+            <div className="container">
+                <div className="about_wrapper">
+                    <div className="about_content">
+                        <h6 className="subtitle">Why Choosing Us</h6>
+                        <h2>We are Team Of Professionals</h2>
+                        <h2 className="highlight">Lets Grow Further Towards Future</h2>
+                        <p className="description about_content-desc">Lorem ipsum. Neque earum quod aspernatur exercitationem illum incidunt harum consectetur.</p>
+                        <div className="choose_item-wrapper">
+                            {
+                                chooseData.map((item, index) => (
+                                    <div className="choose_us-item" key={index}>
+                                        <span className="choose_us-icon"><i class={item.icon}></i></span>
+                                        <div>
+                                            <h4 className="choose_us-title">{item.title}</h4>
+                                            <p className="description">{item.description}</p>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>
 
-                <div className="about_img"><img src={aboutImg} alt="" /></div>
+                    <div className="about_img"><img src={aboutImg} alt="" /></div>
+                </div>
             </div>
-        </div>
-    </section>
-  );
+        </section>
+    );
 };
 
 export default About;
